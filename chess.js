a127acfd2bf5fa4e1f541f6fd89c8ba981387116
Christian Harris <c225h802@ku.edu>
2020-04-06 13:59:05 -0500

    Displaying and styling the pieces of the game on the board

--- chess.js
+++ chess.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     createBoard();
+    setupBoard();
 });
 
 function createBoard() {
@@ -19,5 +20,36 @@ function createBoard() {
         }
 
         theBoard.appendChild(aRow);
+    }
+}
+
+function setupBoard() {
+    let pieceList = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"];
+    for (let i = 97; i <= 104; i++) { // 97 = 'a', 104 = 'h'
+        // Black Player's Pieces
+        let aSquare = document.getElementById(String.fromCharCode(i) + 8);
+        let aBlackPiece = document.createElement("div");
+        aBlackPiece.id = pieceList[i-97] + "B";
+        aBlackPiece.className = "boardPiece";
+        aSquare.appendChild(aBlackPiece);
+
+        aSquare = document.getElementById(String.fromCharCode(i) + 7);
+        let aBlackPawn = document.createElement("div");
+        aBlackPawn.id = "pawnB";
+        aBlackPawn.className = "boardPiece";
+        aSquare.appendChild(aBlackPawn);
+
+        // White Player's Pieces
+        aSquare = document.getElementById(String.fromCharCode(i) + 1);
+        let aWhitePiece = document.createElement("div");
+        aWhitePiece.id = pieceList[i-97] + "W";
+        aWhitePiece.className = "boardPiece";
+        aSquare.appendChild(aWhitePiece);
+
+        aSquare = document.getElementById(String.fromCharCode(i) + 2);
+        let aWhitePawn = document.createElement("div");
+        aWhitePawn.id = "pawnW";
+        aWhitePawn.className = "boardPiece";
+        aSquare.appendChild(aWhitePawn);
     }
 }
