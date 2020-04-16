974098424930127c38bf618720b4d21f15a405ac
Christian Harris <c225h802@ku.edu>
2020-04-15 20:13:04 -0500

    Ability to getPossibleMoves() for knights and kings

--- sources/chess.js
+++ sources/chess.js
@@ -39,11 +39,11 @@ function setupBoard() {
         aBlackPiece.className = "boardPiece";
         aSquare.appendChild(aBlackPiece);
 
-        /* aSquare = document.getElementById(String.fromCharCode(i) + 7);
+        aSquare = document.getElementById(String.fromCharCode(i) + 7);
         let aBlackPawn = document.createElement("div");
         aBlackPawn.id = "pawnB";
         aBlackPawn.className = "boardPiece";
-        aSquare.appendChild(aBlackPawn); */
+        aSquare.appendChild(aBlackPawn);
 
         // White Player's Pieces
         aSquare = document.getElementById(String.fromCharCode(i) + 1);
@@ -52,11 +52,11 @@ function setupBoard() {
         aWhitePiece.className = "boardPiece";
         aSquare.appendChild(aWhitePiece);
 
-        /* aSquare = document.getElementById(String.fromCharCode(i) + 2);
+        aSquare = document.getElementById(String.fromCharCode(i) + 2);
         let aWhitePawn = document.createElement("div");
         aWhitePawn.id = "pawnW";
         aWhitePawn.className = "boardPiece";
-        aSquare.appendChild(aWhitePawn); */
+        aSquare.appendChild(aWhitePawn);
     }
 }
 
@@ -195,7 +195,6 @@ function getPossibleMoves(aBoardPiece) {
         }
     }
     if (pieceType == "bishop" || pieceType == "queen") { // I think it will fail to detect enemy in adjacent boardSquare
-        console.log("hit");
         let evaluatingRow = currentRow + 1;
         let evaluatingColumn = currentColumn + 1;
         let posAntiDirSquare = document.getElementById(String.fromCharCode(evaluatingColumn) + evaluatingRow);
@@ -257,7 +256,192 @@ function getPossibleMoves(aBoardPiece) {
             }
         }
     }
-    
+    if (pieceType == "knight") {
+        let evaluatingRow = currentRow + 2;
+        let posVerticalDirSquareR = document.getElementById(String.fromCharCode(currentColumn + 1) + evaluatingRow);
+        if (posVerticalDirSquareR != null) {
+            if (posVerticalDirSquareR.hasChildNodes()) {
+                if (posVerticalDirSquareR.firstChild.id.charAt(posVerticalDirSquareR.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(posVerticalDirSquareR.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(posVerticalDirSquareR.id);
+            }
+        }
+        let posVerticalDirSquareL = document.getElementById(String.fromCharCode(currentColumn - 1) + evaluatingRow);
+        if (posVerticalDirSquareL != null) {
+            if (posVerticalDirSquareL.hasChildNodes()) {
+                if (posVerticalDirSquareL.firstChild.id.charAt(posVerticalDirSquareL.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(posVerticalDirSquareL.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(posVerticalDirSquareL.id);
+            }
+        }
+        evaluatingRow = currentRow - 2;
+        let negVerticalDirSquareR = document.getElementById(String.fromCharCode(currentColumn + 1) + evaluatingRow);
+        if (negVerticalDirSquareR != null) {
+            if (negVerticalDirSquareR.hasChildNodes()) {
+                if (negVerticalDirSquareR.firstChild.id.charAt(negVerticalDirSquareR.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(negVerticalDirSquareR.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(negVerticalDirSquareR.id);
+            }
+        }
+        let negVerticalDirSquareL = document.getElementById(String.fromCharCode(currentColumn - 1) + evaluatingRow);
+        if (negVerticalDirSquareL != null) {
+            if (negVerticalDirSquareL.hasChildNodes()) {
+                if (negVerticalDirSquareL.firstChild.id.charAt(negVerticalDirSquareL.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(negVerticalDirSquareL.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(negVerticalDirSquareL.id);
+            }
+        }
+
+
+        let evaluatingColumn = currentColumn + 2;
+        let posHorizontalDirSquareT = document.getElementById(String.fromCharCode(evaluatingColumn) + (currentRow + 1));
+        if (posHorizontalDirSquareT != null) {
+            if (posHorizontalDirSquareT.hasChildNodes()) {
+                if (posHorizontalDirSquareT.firstChild.id.charAt(posHorizontalDirSquareT.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(posHorizontalDirSquareT.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(posHorizontalDirSquareT.id);
+            }
+        }
+        let posHorizontalDirSquareB = document.getElementById(String.fromCharCode(evaluatingColumn) + (currentRow - 1));
+        if (posHorizontalDirSquareB != null) {
+            if (posHorizontalDirSquareB.hasChildNodes()) {
+                if (posHorizontalDirSquareB.firstChild.id.charAt(posHorizontalDirSquareB.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(posHorizontalDirSquareB.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(posHorizontalDirSquareB.id);
+            }
+        }
+        evaluatingColumn = currentColumn - 2;
+        let negHorizontalDirSquareT = document.getElementById(String.fromCharCode(evaluatingColumn) + (currentRow + 1));
+        if (negHorizontalDirSquareT != null) {
+            if (negHorizontalDirSquareT.hasChildNodes()) {
+                if (negHorizontalDirSquareT.firstChild.id.charAt(negHorizontalDirSquareT.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(negHorizontalDirSquareT.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(negHorizontalDirSquareT.id);
+            }
+        }
+        let negHorizontalDirSquareB = document.getElementById(String.fromCharCode(evaluatingColumn) + (currentRow - 1));
+        if (negHorizontalDirSquareB != null) {
+            if (negHorizontalDirSquareB.hasChildNodes()) {
+                if (negHorizontalDirSquareB.firstChild.id.charAt(negHorizontalDirSquareB.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(negHorizontalDirSquareB.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(negHorizontalDirSquareB.id);
+            }
+        }
+    }
+    if (pieceType == "king") {
+        let topSquare = document.getElementById(String.fromCharCode(currentColumn) + (currentRow + 1));
+        if (topSquare != null) {
+            if (topSquare.hasChildNodes()) {
+                if (topSquare.firstChild.id.charAt(topSquare.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(topSquare.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(topSquare.id);
+            }
+        }
+        let topRightSquare = document.getElementById(String.fromCharCode(currentColumn + 1) + (currentRow + 1));
+        if (topRightSquare != null) {
+            if (topRightSquare.hasChildNodes()) {
+                if (topRightSquare.firstChild.id.charAt(topRightSquare.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(topRightSquare.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(topRightSquare.id);
+            }
+        }
+        let topLeftSquare = document.getElementById(String.fromCharCode(currentColumn - 1) + (currentRow + 1));
+        if (topLeftSquare != null) {
+            if (topLeftSquare.hasChildNodes()) {
+                if (topLeftSquare.firstChild.id.charAt(topLeftSquare.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(topLeftSquare.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(topLeftSquare.id);
+            }
+        }
+        let leftSquare = document.getElementById(String.fromCharCode(currentColumn - 1) + (currentRow));
+        if (leftSquare != null) {
+            if (leftSquare.hasChildNodes()) {
+                if (leftSquare.firstChild.id.charAt(leftSquare.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(leftSquare.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(leftSquare.id);
+            }
+        }
+        let rightSquare = document.getElementById(String.fromCharCode(currentColumn + 1) + (currentRow));
+        if (rightSquare != null) {
+            if (rightSquare.hasChildNodes()) {
+                if (rightSquare.firstChild.id.charAt(rightSquare.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(rightSquare.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(rightSquare.id);
+            }
+        }
+        let bottomSquare = document.getElementById(String.fromCharCode(currentColumn) + (currentRow - 1));
+        if (bottomSquare != null) {
+            if (bottomSquare.hasChildNodes()) {
+                if (bottomSquare.firstChild.id.charAt(bottomSquare.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(bottomSquare.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(bottomSquare.id);
+            }
+        }
+        let bottomRightSquare = document.getElementById(String.fromCharCode(currentColumn + 1) + (currentRow - 1));
+        if (bottomRightSquare != null) {
+            if (bottomRightSquare.hasChildNodes()) {
+                if (bottomRightSquare.firstChild.id.charAt(bottomRightSquare.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(bottomRightSquare.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(bottomRightSquare.id);
+            }
+        }
+        let bottomLeftSquare = document.getElementById(String.fromCharCode(currentColumn - 1) + (currentRow - 1));
+        if (bottomLeftSquare != null) {
+            if (bottomLeftSquare.hasChildNodes()) {
+                if (bottomLeftSquare.firstChild.id.charAt(bottomLeftSquare.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(bottomLeftSquare.id);
+                }
+            }
+            else { // boardSquare does not contain a piece
+                possibleMoves.push(bottomLeftSquare.id);
+            }
+        }
+    }
     console.log(possibleMoves);
 
     return possibleMoves;
