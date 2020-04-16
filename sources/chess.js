ea5901baf4c247d6de9861976ac21ff226d4f9a5
Christian Harris <c225h802@ku.edu>
2020-04-15 19:07:47 -0500

    Ability to getPossibleMoves() for rooks

--- sources/chess.js
+++ sources/chess.js
@@ -39,11 +39,11 @@ function setupBoard() {
         aBlackPiece.className = "boardPiece";
         aSquare.appendChild(aBlackPiece);
 
-        aSquare = document.getElementById(String.fromCharCode(i) + 7);
+        /* aSquare = document.getElementById(String.fromCharCode(i) + 7);
         let aBlackPawn = document.createElement("div");
         aBlackPawn.id = "pawnB";
         aBlackPawn.className = "boardPiece";
-        aSquare.appendChild(aBlackPawn);
+        aSquare.appendChild(aBlackPawn); */
 
         // White Player's Pieces
         aSquare = document.getElementById(String.fromCharCode(i) + 1);
@@ -52,39 +52,12 @@ function setupBoard() {
         aWhitePiece.className = "boardPiece";
         aSquare.appendChild(aWhitePiece);
 
-        aSquare = document.getElementById(String.fromCharCode(i) + 2);
+        /* aSquare = document.getElementById(String.fromCharCode(i) + 2);
         let aWhitePawn = document.createElement("div");
         aWhitePawn.id = "pawnW";
         aWhitePawn.className = "boardPiece";
-        aSquare.appendChild(aWhitePawn);
+        aSquare.appendChild(aWhitePawn); */
     }
-}
-
-function getPossibleMoves(aBoardPiece) {
-    let currentLocation = aBoardPiece.parentNode.id;
-    let currentColumn = currentLocation.charAt(0).charCodeAt(0);
-    let currentRow = parseInt(currentLocation.charAt(1));
-
-    let pieceType = aBoardPiece.id.substring(0, aBoardPiece.id.length - 1);
-    let possibleMoves = [];
-
-    if (pieceType == "pawn") {
-        let pieceColor = aBoardPiece.id.charAt(aBoardPiece.id.length - 1);
-        if (pieceColor == "W") {
-            possibleMoves.push(String.fromCharCode(currentColumn) + (currentRow + 1));
-            if (currentRow == 2) {
-                possibleMoves.push(String.fromCharCode(currentColumn) + (currentRow + 2));
-            }
-        }
-        else {
-            possibleMoves.push(String.fromCharCode(currentColumn) + (currentRow - 1));
-            if (currentRow == 7) {
-                possibleMoves.push(String.fromCharCode(currentColumn) + (currentRow - 2));
-            }
-        }
-    }
-
-    return possibleMoves;
 }
 
 function showMoves(possibleMoves) {
@@ -95,5 +68,134 @@ function showMoves(possibleMoves) {
         possibleMoves.forEach((move) => {
             document.getElementById(move).classList.remove("move");
         })
-    }, 2500);
+    }, 2000);
+}
+
+function getPossibleMoves(aBoardPiece) {
+    let currentLocation = aBoardPiece.parentNode.id;
+    let currentColumn = currentLocation.charCodeAt(0);
+    let currentRow = parseInt(currentLocation.charAt(1));
+
+    let pieceType = aBoardPiece.id.substring(0, aBoardPiece.id.length - 1);
+    let pieceColor = aBoardPiece.id.charAt(aBoardPiece.id.length - 1);
+
+    let possibleMoves = [];
+
+    if (pieceType == "pawn") {
+
+        if (pieceColor == "W") { // a white pawn
+            let facingBoardSquare = document.getElementById(String.fromCharCode(currentColumn) + (currentRow + 1));
+            // if the boardSquare in front of the pawn does not contain a piece
+            if (!facingBoardSquare.hasChildNodes()) {
+                possibleMoves.push(facingBoardSquare.id);
+            }
+            let antiBoardSquare = document.getElementById(String.fromCharCode(currentColumn + 1) + (currentRow + 1));
+            // if a black piece can be attacked on the anti diagonal
+            if (antiBoardSquare != null && antiBoardSquare.hasChildNodes()) {
+                if (antiBoardSquare.firstChild.id.charAt(antiBoardSquare.id.length - 1) != pieceColor) {
+                    possibleMoves.push(antiBoardSquare.id);
+                }
+            }
+            let mainBoardSquare = document.getElementById(String.fromCharCode(currentColumn - 1) + (currentRow + 1));
+            // if a black piece can be attacked on the main diagonal
+            if (mainBoardSquare != null && mainBoardSquare.hasChildNodes()) {
+                if (mainBoardSquare.firstChild.id.charAt(mainBoardSquare.length - 1) != pieceColor) {
+                    possibleMoves.push(mainBoardSquare.id);
+                }
+            }
+            let twoAwayFacingBoardSquare = document.getElementById(String.fromCharCode(currentColumn) + (currentRow + 2));
+            // if the pawn is at its starting position
+            if (!twoAwayFacingBoardSquare.hasChildNodes() && currentRow == 2) {
+                possibleMoves.push(twoAwayFacingBoardSquare.id);
+            }
+        }
+        else { // a black pawn
+            let facingBoardSquare = document.getElementById(String.fromCharCode(currentColumn) + (currentRow - 1));
+            // if the boardSquare in front of the pawn does not contain a piece
+            if (!facingBoardSquare.hasChildNodes()) {
+                possibleMoves.push(facingBoardSquare.id);
+            }
+            let antiBoardSquare = document.getElementById(String.fromCharCode(currentColumn - 1) + (currentRow - 1));
+            // if a white piece can be attacked on the anti diagonal
+            if (antiBoardSquare != null && antiBoardSquare.hasChildNodes()) {
+                if (antiBoardSquare.firstChild.id.charAt(antiBoardSquare.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(antiBoardSquare.id);
+                }
+            }
+            let mainBoardSquare = document.getElementById(String.fromCharCode(currentColumn + 1) + (currentRow - 1));
+            // if a white piece can be attacked on the main diagonal
+            if (mainBoardSquare != null && mainBoardSquare.hasChildNodes()) {
+                if (mainBoardSquare.firstChild.id.charAt(mainBoardSquare.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(mainBoardSquare.id);
+                }
+            }
+            let twoAwayFacingBoardSquare = document.getElementById(String.fromCharCode(currentColumn) + (currentRow - 2));
+            // if the pawn is at its starting position
+            if (!twoAwayFacingBoardSquare.hasChildNodes() && currentRow == 7) {
+                possibleMoves.push(twoAwayFacingBoardSquare.id);
+            }
+        }
+    }
+    else if (pieceType == "rook") { // I think it will fail to detect enemy in adjacent boardSquare
+        let evaluatingRow = currentRow + 1;
+        let posVerticalDirSquare = document.getElementById(String.fromCharCode(currentColumn) + evaluatingRow);
+        // while the next boardSquare is not off the board and does not contain a piece
+        while (posVerticalDirSquare != null && !posVerticalDirSquare.hasChildNodes()) {
+            possibleMoves.push(posVerticalDirSquare.id);
+            posVerticalDirSquare = document.getElementById(String.fromCharCode(currentColumn) + ++evaluatingRow);
+            // if the next boardSquare is not off the board and contains a piece
+            if (posVerticalDirSquare != null && posVerticalDirSquare.hasChildNodes()) {
+                // if the piece is not the same color as the moving rook
+                if (posVerticalDirSquare.firstChild.id.charAt(posVerticalDirSquare.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(posVerticalDirSquare.id);
+                }
+            }
+        }
+        evaluatingRow = currentRow - 1;
+        let negVerticalDirSquare = document.getElementById(String.fromCharCode(currentColumn) + evaluatingRow);
+        // while the next boardSquare is not off the board and does not contain a piece
+        while (negVerticalDirSquare != null && !negVerticalDirSquare.hasChildNodes()) {
+            possibleMoves.push(negVerticalDirSquare.id);
+            negVerticalDirSquare = document.getElementById(String.fromCharCode(currentColumn) + --evaluatingRow);
+            // if the next boardSquare is not off the board and contains a piece
+            if (negVerticalDirSquare != null && negVerticalDirSquare.hasChildNodes()) {
+                // if the piece is not the same color as the moving rook
+                if (negVerticalDirSquare.firstChild.id.charAt(negVerticalDirSquare.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(negVerticalDirSquare.id);
+                }
+            }
+        }
+        let evaluatingColumn = currentColumn + 1;
+        let posHorizontalDirSquare = document.getElementById(String.fromCharCode(evaluatingColumn) + currentRow);
+        // while the next boardSquare is not off the board and does not contain a piece
+        while (posHorizontalDirSquare != null && !posHorizontalDirSquare.hasChildNodes()) {
+            possibleMoves.push(posHorizontalDirSquare.id);
+            posHorizontalDirSquare = document.getElementById(String.fromCharCode(++evaluatingColumn) + currentRow);
+            // if the next boardSquare is not off the board and contains a piece
+            if (posHorizontalDirSquare != null && posHorizontalDirSquare.hasChildNodes()) {
+                // if the piece is not the same color as the moving rook
+                if (posHorizontalDirSquare.firstChild.id.charAt(posHorizontalDirSquare.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(posHorizontalDirSquare.id);
+                }
+            }
+        }
+        evaluatingColumn = currentColumn - 1;
+        let negHorizontalDirSquare = document.getElementById(String.fromCharCode(evaluatingColumn) + currentRow);
+        // while the next boardSquare is not off the board and does not contain a piece
+        while (negHorizontalDirSquare != null && !negHorizontalDirSquare.hasChildNodes()) {
+            possibleMoves.push(negHorizontalDirSquare.id);
+            negHorizontalDirSquare = document.getElementById(String.fromCharCode(--evaluatingColumn) + currentRow);
+            // if the next boardSquare is not off the board and contains a piece
+            if (negHorizontalDirSquare != null && negHorizontalDirSquare.hasChildNodes()) {
+                // if the piece is not the same color as the moving rook
+                if (negHorizontalDirSquare.firstChild.id.charAt(negHorizontalDirSquare.firstChild.id.length - 1) != pieceColor) {
+                    possibleMoves.push(negHorizontalDirSquare.id);
+                }
+            }
+        }
+    }
+
+    console.log(possibleMoves);
+
+    return possibleMoves;
 }
