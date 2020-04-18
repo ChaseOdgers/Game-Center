d2fe6b5d897712b8faae171898ab77430ce0ffb3
Kerr Salabao <kerr712@gmail.com>
2020-04-18 14:11:54 -0500

    Fixed the starting menu
    Maid the starting menu disappear when the type of student has been selected

--- sources/tDefense/gameGeneration.js
+++ sources/tDefense/gameGeneration.js
@@ -1,31 +1,42 @@
-document.getElementById("aStudent").addEventListener("click", function(){
+document.addEventListener("DOMContentLoaded",() => {
+
+  document.getElementById("aStudent").addEventListener("click", function(){
+
+    document.getElementById("startingMenu").remove();
+    document.getElementById("mainHeading").innerHTML = "The Good Student";
+
+
+
+  });
+
+  document.getElementById("cStudent").addEventListener("click", function(){
+
+    document.getElementById("startingMenu").remove();
+    document.getElementById("mainHeading").innerHTML = "The Good Student";
 
 
 
 
+  });
 
-});
+  document.getElementById("partyAnimal").addEventListener("click", function(){
 
-document.getElementById("cStudent").addEventListener("click", function(){
-
-
-
-
-
-});
-
-document.getElementById("partyAnimal").addEventListener("click", function(){
+    document.getElementById("startingMenu").remove();
+    document.getElementById("mainHeading").innerHTML = "The Good Student";
 
 
 
 
+  });
 
-});
+  document.getElementById("engineeringStudent").addEventListener("click", function(){
 
-document.getElementById("engineeringStudent").addEventListener("click", function(){
+    document.getElementById("startingMenu").remove();
+    document.getElementById("mainHeading").innerHTML = "The Good Student";
 
 
 
 
+  });
 
 });
