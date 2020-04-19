59f9f5c82b23c8a47445142f88ca0160c99b3ccf
Kerr Salabao <kerr712@gmail.com>
2020-04-18 21:16:53 -0500

    Created the student prototype

---
+++ sources/tDefense/studentActions.js
@@ -0,0 +1,15 @@
+function student(type, currentGrade, MH, PH, sleep, social, study){
+  this.type = type;
+  this.grade = currentGrade;
+  this.mentalHealth = MH;
+  this.physicalHealth = PH;
+  this.sleep = sleep;
+  this.social = social;
+  this.study = study;
+
+  let adversaries = [];
+  let smallCommits = [];
+  let mediumCommits = [];
+  let bigCommits = [];
+  let lifeEvets = [];
+}
