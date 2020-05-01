9353a4574c5c1ba3cab55317fb731853fe7f8fae
Brian Jones <bjones033@gmail.com>
2020-05-01 14:25:28 -0500

    Documentation - Battleship generateBoard.js

--- sources/battleship/generateBoard.js
+++ sources/battleship/generateBoard.js
@@ -1,3 +1,7 @@
+/** Creates a 10x10 table from a given table id.
+    @pre    Valid table id.
+    @param  id The id of the HTML table element.
+    @post   The HTML table element is filled with 10 tr's x 10 td's. */
 function createTable(id) {
     for (let row = 0; row < 10; row++)
     {
